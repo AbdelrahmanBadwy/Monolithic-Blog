e6c5522327d21546b4599b1b6fb6b598c9026ae1
Abdelrahman El-Badawy <abdoo738@yahoo.com>
2025-05-07 13:50:45 +0300

    feat: added post model and db connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
@@ -20,4 +21,14 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://abdalrmanbadwy:Bgz4qiNKhZNMPPtt@cluster0.izjfuje.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((result) => {
+    console.log("Connected to database!");
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,5 +1,5 @@
 const { validationResult } = require("express-validator");
-
+const Post = require("../models/post");
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -30,15 +30,38 @@ exports.createPost = (req, res, next) => {
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "Abdelrahman El Badawy" },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/duck.jpg",
+    creator: { name: "Abdelrahman El Badawy" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "Creating a post failed!",
+        error: err,
+      });
+    });  
+    //=============================================
+  // // Create post in db
+  // res.status(201).json({
+  //   message: "Post created successfully!",
+  //   post: {
+  //     _id: new Date().toISOString(),
+  //     title: title,
+  //     content: content,
+  //     creator: { name: "Abdelrahman El Badawy" },
+  //     createdAt: new Date(),
+  //   },
+  // });
 };
